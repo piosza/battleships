96d178fb243ec2a82edd14f417a0ce9623956ed1
Piosza <piosza@tlen.pl>
2023-04-15 17:07:34 +0200

    class  board

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 # main game code
+import random
+import os
+from colorama import init, Fore, Back, Style
+
+
+os.system("cls")
+print()
+
+class Board:
+    def __init__(self):
+    Board = ['_' for i in range(100)]
+ 
