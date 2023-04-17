b7978619545447cc410e80d79746d4abf5ea5e3b
Piosza <piosza@tlen.pl>
2023-04-17 20:05:10 +0200

    created board printing

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # main game code
 import random
 import os
-from colorama import init, Fore, Back, Style
+#from colorama import init, Fore, Back, Style
 
 
 os.system("cls")
@@ -9,5 +9,32 @@ print()
 
 class Board:
     def __init__(self):
-    Board = ['_' for i in range(100)]
- 
+        self.state = [['_' for i in range(10)] for j in range(10)]
+#board1 =  Board()
+#board1.state [5][5] = "x"
+#print  (board1.state)
+    def __str__(self):
+        return  """
+      0 1 2 3 4 5 6 7 8 9
+    0|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|
+    1|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|
+    2|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|
+    3|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|
+    4|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|
+    5|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|
+    6|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|
+    7|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|
+    8|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|
+    9|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|
+    """.format(
+        *sum(self.state, [])
+    )
+board1 =  Board()
+
+print  (board1)
+
+while True:
+    x= int(input())
+    y= int(input())
+    board1.state[x][y] = "#"
+    print  (board1)
